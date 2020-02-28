9f9b82fb5b6372707fc08a665044f854996048f9
Shashank Gupta <shashank.gupta@talentica.com>
2020-02-28 11:45:05 +0530

    Create and Get all Tickets

--- main/model_form/ticket_form.py
+++ main/model_form/ticket_form.py
@@ -1,8 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from main.models.ticket import Ticket
 
 
-class TicketForm(ModelForm):
+class TicketForm(forms.ModelForm):
+    pickup_time = forms.DateTimeField(
+        widget=forms.widgets.DateTimeInput(attrs={'type': 'datetime-local'}),
+    )
+
     class Meta:
         model = Ticket
         fields = ["pickup_location", "pickup_time", "drop_location", "number_of_seats", "ac_required"]
--- main/models/ticket.py
+++ main/models/ticket.py
@@ -1,20 +1,20 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Ticket(models.Model):
     id = models.AutoField(primary_key=True)
-   # created_by = models.ForeignKey(settings.AUTH_USER_MODEL)
+    #created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     pickup_location = models.CharField(max_length=255)
     pickup_time = models.DateTimeField()
     drop_location = models.CharField(max_length=255)
-    number_of_seats = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(6)])
+    number_of_seats = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(8)])
     ac_required = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     total_fare = models.DecimalField(max_digits=5, decimal_places=2)
     distance_travelled = models.DecimalField(max_digits=4, decimal_places=2)
-    cab_feedback = models.CharField(max_length=200)
+    cab_feedback = models.CharField(max_length=255)
 
     class Meta:
         db_table = "ticket"
