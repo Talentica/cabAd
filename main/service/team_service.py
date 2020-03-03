e1f1750cc19f25410f42d3bdf16de82e808a6789
Sushant <sushant.dey@talentica.com>
2020-03-03 17:01:57 +0530

    add logging and redirect to get_team_by_id view after create_team

--- main/service/team_service.py
+++ main/service/team_service.py
@@ -2,6 +2,10 @@ from django import forms
 
 from main.model_form.team_form import TeamForm
 from main.models.team import Team
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 def create(team):
@@ -10,24 +14,24 @@ def create(team):
         if form.is_valid():
             return form.save()
         else:
+            logger.error("Invalid Team form...!!!")
             raise
-            print("form not valid")
-    except:
-        print("exception in service")
+    except Exception as e:
+        logger.error("Team Service: Exception while creating new team...!!!", e)
         raise
 
 
 def get_team(team_id):
     try:
         return Team.objects.get(id=team_id)
-    except:
-        print("exception in service")
+    except Exception as e:
+        logger.error("Team Service: Exception while getting team by id...!!!", e)
         raise
 
 
 def get_all_teams():
     try:
         return Team.objects.all()
-    except:
-        print("exception in service")
+    except Exception as e:
+        logger.error("Team Service: Exception while getting all teams...!!!", e)
         raise
