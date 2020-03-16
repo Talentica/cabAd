6acce4c43ec7eb1067f67aee22bf40c49cb227bd
Shashank Gupta <shashank.gupta@talentica.com>
2020-03-16 11:49:16 +0530

    Create and get all ticket

--- main/controller/ticket_controller.py
+++ main/controller/ticket_controller.py
@@ -1,6 +1,6 @@
 from main.model_form.ticket_form import TicketForm
 from main.service import ticket_service
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 import logging
 
@@ -12,14 +12,17 @@ class TicketView(View):
     def get(self, request):
         try:
             return render(request, 'ticket/tickets.html', {'tickets': ticket_service.all_tickets()})
-        except:
-            logger.error("Exception in getting ticket view")
+        except Exception as e:
+            logger.error("Exception in getting ticket view", e)
+            return redirect("create_ticket")
 
     def post(self, request):
         try:
-            return render(request, 'ticket/ticket.html', {'ticket': ticket_service.create(request.POST)})
-        except:
-            logger.error("Exception in post ticket view")
+            ticket = ticket_service.create(request.POST)
+            return redirect('get_ticket_by_id', id=ticket.id)
+        except Exception as e:
+            logger.error("Exception in post ticket view", e)
+            return redirect("create_ticket")
 
 
 class TicketByIdView(View):
@@ -27,8 +30,9 @@ class TicketByIdView(View):
     def get(self, request, id):
         try:
             return render(request, 'ticket/ticket.html', {'ticket': ticket_service.get_ticket(id)})
-        except:
-            logger.error("Exception in getting ticket by id")
+        except Exception as e:
+            logger.error("Exception in getting ticket by id", e)
+            return redirect("create_or_get_all_tickets")
 
 
 def create_ticket(request):
